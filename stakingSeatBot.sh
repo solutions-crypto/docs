6074305f7428dc3fcc8eea5e5185432594f7994e
crypto-solutions <74749660+crypto-solutions-net@users.noreply.github.com>
2021-01-06 18:06:25 -0600

    Update stakingSeatBot.sh

--- stakingSeatBot.sh
+++ stakingSeatBot.sh
@@ -2,36 +2,66 @@
 PATH="/usr/local/bin:/usr/bin:/bin"
 PATH=$PATH
 DEBUG_ALL=0
-DEBUG_MIN=1
+DEBUG_MIN=0
 
-NETWORK="guildnet"
-GUILDNET_EPOCH_LEN=5000
-BETANET_EPOCH_LEN=10000
-TESTNET=43200
-MAINET=43200
-ADD0=000000000000000000000000
-
-export NEAR_ENV=$NETWORK
-
-HOST="https://rpc.openshards.io"
-POOL_ID="???.stake.guildnet"
-pool_id1='"???.stake.guildnet"'
-ACCOUNT_ID="???.guildnet"
-#PUBLIC_KEY=""
-SEAT_PRICE_BUFFER=4000
+# SETTINGS
+NETWORK="???"
+POOL_ID="???"
+pool_id1='"???"'
+ACCOUNT_ID="???"
 NUM_SEATS_TO_OCCUPY=???
 
-PARAMS='{"jsonrpc": "2.0", "method": "validators", "id": "dontcare", "params": [null]}'
-CT='Content-Type: application/json'
+# Epoch Lengths
+GUILDNET_EPOCH_LEN=5000
+BETANET_EPOCH_LEN=10000
+TESTNET_EPOCH_LEN=43200
+MAINET_EPOCH_LEN=43200
 
+# Additional Script Configuration
+ADD0=000000000000000000000000
+SEAT_PRICE_BUFFER=10000
+#PARAMS='{"jsonrpc": "2.0", "method": "validators", "id": "dontcare", "params": [null]}'
+#CT='Content-Type: application/json'
 COMMA=","
-DOUBLE_QUOTE="\""
+#DOUBLE_QUOTE="\""
+#PUBLIC_KEY=""
 
+# Export the network to the environment
+export NEAR_ENV=$NETWORK
+
+# Select the correct RPC server for the network
+case $NETWORK in
+
+  guildnet)
+    HOST="https://rpc.openshards.io"
+    ;;
+
+  mainnet)
+    HOST="https://rpc.near.org"
+    ;;
+
+  testnet)
+    HOST="https://rpc.testnet.near.org/"
+    ;;
+
+  betanet)
+    HOST="https://rpc.betanet.near.org/"
+    ;;
+
+  *)
+    ;;
+esac
 
 echo "Starting Script"
 echo "---------------"
+
+# Ensure user has configured the script
+if [ "$POOL_ID" == "???" ]
+then
+echo "You have not properly configured this script. Please edit the file and replace every instance of ??? with valid data"
+fi
+
 VALIDATORS=$(curl -s -d '{"jsonrpc": "2.0", "method": "validators", "id": "dontcare", "params": [null]}' -H 'Content-Type: application/json' $HOST )
-#VALIDATORS=$(curl -s -d "'$PARAMS'" -H "$CT" "$HOST")
 if [ "$DEBUG_ALL" == "1" ]
 then
   echo "Validators: $VALIDATORS"
@@ -47,7 +77,6 @@ fi
 EPOCH_START=$(echo "$VALIDATORS" | jq .result.epoch_start_height)
 if [ "$DEBUG_MIN" == "1" ]
 then
-  echo "$VALIDATORS" 
   echo "Epoch start: $EPOCH_START"
 fi
 
@@ -57,30 +86,63 @@ then
   echo "Last Block: $LAST_BLOCK"
 fi
 
+# Calculate blocks and time remaining in epoch based on the network selected
 BLOCKS_COMPLETED=$((LAST_BLOCK - EPOCH_START))
-BLOCKS_REMAINING=$((BLOCKS_COMPLETED - GUILDNET_EPOCH_LEN))
+
+case $NETWORK in
+
+  guildnet)
+    BLOCKS_REMAINING=$((BLOCKS_COMPLETED - GUILDNET_EPOCH_LEN))
+    EPOCH_MINS_REMAINING=$((BLOCKS_REMAINING / 60))
+    ;;
+
+  mainnet)
+    BLOCKS_REMAINING=$((BLOCKS_COMPLETED - MAINET_EPOCH_LEN))
+    EPOCH_MINS_REMAINING=$((BLOCKS_REMAINING / 60))
+    ;;
+
+  testnet)
+    BLOCKS_REMAINING=$((BLOCKS_COMPLETED - TESTNET_EPOCH_LEN))
+    EPOCH_MINS_REMAINING=$((BLOCKS_REMAINING / 60))
+    ;;
+
+  betanet)
+    BLOCKS_REMAINING=$((BLOCKS_COMPLETED - BETANET_EPOCH_LEN))
+    EPOCH_MINS_REMAINING=$((BLOCKS_REMAINING / 60))
+    ;;
+
+  *)
+    ;;
+esac
+
 if [ "$DEBUG_MIN" == "1" ]
 then
-  echo "Blocks Completed: $BLOCKS_COMPLETED"
-  echo "Blocks Remaining: $BLOCKS_REMAINING"
+echo "Blocks Completed: $BLOCKS_COMPLETED"
+echo "Blocks Remaining: $BLOCKS_REMAINING"
+echo "Epoch Minutes Remaining: $EPOCH_MINS_REMAINING"
 fi
 
 CURRENT_STAKE_S=$(echo "$VALIDATORS" | jq -c '.result.current_validators[] | select(.account_id | contains ('$pool_id1'))' | jq .stake)
 CURRENT_STAKE_L=(${CURRENT_STAKE_S//\"/})
 CURRENT_STAKE="${CURRENT_STAKE_L%????????????????????????}"
+
+if [[ "$DEBUG_MIN" == "1" && -z "$CURRENT_STAKE_S" ]]
+then
+  echo "$POOL_ID is not listed in the proposals for the current epoch"
+fi
+
 if [[ "$DEBUG_MIN" == "1" && "$CURRENT_STAKE_S" ]]
 then
   echo "Current Stake: $CURRENT_STAKE"
-  echo "Current Stake: $CURRENT_STAKE_S"
-  echo "Current Stake: $CURRENT_STAKE_L"
-  else
-  echo "$POOL_ID is not listed in the proposals for the current epoch"
+  echo "Current Stake_S: $CURRENT_STAKE_S"
+  echo "Current Stake_L: $CURRENT_STAKE_L"
 fi
 
 VALIDATOR_NEXT_STAKE_S=$(echo "$VALIDATORS" | jq -c ".result.next_validators[] | select(.account_id | contains (\"$POOL_ID\"))" | jq .stake)
 VALIDATOR_NEXT_STAKE_L=(${VALIDATOR_NEXT_STAKE_S//\"/})
 VALIDATOR_NEXT_STAKE="${VALIDATOR_NEXT_STAKE_L%????????????????????????}"
-if [ -z "$VALIDATOR_NEXT_STAKE" ]
+
+if [[ "$DEBUG_MIN" == "1" && -z "$VALIDATOR_NEXT_STAKE" ]]
 then
   echo "$POOL_ID is not listed in the proposals for the next epoch"
 fi
@@ -91,50 +153,65 @@ then
   echo "Next Stake Long: $VALIDATOR_NEXT_STAKE_L"
 fi
 
-
 KICK_REASON=$(echo "$VALIDATORS" | jq -c ".result.prev_epoch_kickout[] | select(.account_id | contains (\"$POOL_ID\"))" | jq .reason)
+if [[ "$KICK_REASON" && "$DEBUG_MIN" == "1" ]]
+then
+    echo "Validator Kicked Reason = $KICK_REASON"
+    PING_COMMAND=$(near call $POOL_ID ping "{}" --accountId $ACCOUNT_ID)
+    echo "$PING_COMMAND"
+    exit
+fi
+if [[ "$KICK_REASON" && "$DEBUG_MIN" == "0" ]]
+then
+    echo "Validator was Kicked Pinging"
+    echo "$PING_COMMAND"
+    exit
+fi
 
+# Proposal for epoch +2
 OUR_PROPOSAL=$(echo "$VALIDATORS" | jq -c ".result.current_proposals[] | select(.account_id | contains (\"$POOL_ID\"))" | jq .stake)
 PROPOSAL_STAKE_S=$(echo "$VALIDATORS" | jq -c ".result.current_proposals[] | select(.account_id | contains (\"$POOL_ID\"))" | jq .stake)
 PROPOSAL_STAKE=${PROPOSAL_STAKE_S//\"/}
 PROPOSAL_STAKE="${PROPOSAL_STAKE_S%?????????????????????????}"
+
 if [[ -z "$OUR_PROPOSAL" ]]
 then
 echo "We dont have a proposal sending a ping"
 PING_COMMAND=$(near call $POOL_ID ping "{}" --accountId $ACCOUNT_ID)
 echo "$PING_COMMAND"
 exit
-else
+fi
+
 OUR_PROPOSAL_S=$(echo $OUR_PROPOSAL | sed 's/[^0-9]*//g')
 PROPOSAL_STAKE=$(echo $PROPOSAL_STAKE | sed 's/[^0-9]*//g')
+if [[ "$PROPOSAL_STAKE" && "$DEBUG_MIN" == "1" ]]
+then
 echo "Our Proposal: $OUR_PROPOSAL"
 echo "Our Proposal_S: $OUR_PROPOSAL_S"
-echo "Our Proposal Stake: $PROPOSAL_STAKE"
-#echo $VALIDATORS | jq -c ".result.current_proposals[]"
+echo "Proposal Stake: $PROPOSAL_STAKE"
+fi
+
+if [ "$DEBUG_ALL" == "1" ]
+then
+echo $VALIDATORS | jq -c ".result.current_proposals[]"
+fi
+
 PROPOSAL_REASON=$(echo "$VALIDATORS" | jq -c ".result.current_proposals[] | select(.account_id | contains (\"$POOL_ID\"))" | jq .reason)
+if [[ "$PROPOSAL_REASON" && "$DEBUG_MIN" == "1" ]]
+then
 echo Proposal Reason: "$PROPOSAL_REASON"
 fi
 
-
-# Change
-# If the validators stake is null in all 3 of these lists proposals, current, and next   then execute function to send proposal
-# If stake is not null check if current proposal is rollover?
-# if yes then ping 
-# FIX IT 
-
-if [[ "$KICK_REASON" ]]
-then
-    echo Validator Kicked Reason = "$KICK_REASON"
-fi
-
+# Current Epoch Seat Price
 CURRENT_SEAT_PRICE=$(near validators current | awk '/price/ {print substr($6, 1, length($6)-2)}')
 CURRENT_SEAT_PRICE="${CURRENT_SEAT_PRICE/$COMMA/}"
 CURRENT_SEAT_PRICE=$((CURRENT_SEAT_PRICE+SEAT_PRICE_BUFFER))
-if [ "$DEBUG_MIN" == "1" ]
+if [[ "$DEBUG_MIN" == "1" && "$CURRENT_SEAT_PRICE" ]]
 then
   echo "Current Epoch Seat Price: $CURRENT_SEAT_PRICE"
 fi
 
+# Next Epoch Seat Price
 SEAT_PRICE_NEXT=$(near validators next | awk '/price/ {print substr($7, 1, length($7)-2)}')
 SEAT_PRICE_NEXT="${SEAT_PRICE_NEXT/$COMMA/}"
 SEAT_PRICE_NEXT=$((SEAT_PRICE_NEXT * NUM_SEATS_TO_OCCUPY))
@@ -145,8 +222,9 @@ fi
 
 SEAT_PRICE_PROPOSALS=$(near proposals | awk '/price =/ {print substr($15, 1, length($15)-1)}')
 SEAT_PRICE_PROPOSALS="${SEAT_PRICE_PROPOSALS/$COMMA/}"
-SEAT_PRICE_PROPOSALS=$((SEAT_PRICE_PROPOSALS * NUM_SEATS_TO_OCCUPY))
 SEAT_PRICE_PROPOSALS=$((SEAT_PRICE_PROPOSALS + SEAT_PRICE_BUFFER))
+SEAT_PRICE_PROPOSALS=$((SEAT_PRICE_PROPOSALS * NUM_SEATS_TO_OCCUPY))
+
 
 if [ "$DEBUG_MIN" == "1" ]
 then
@@ -164,7 +242,7 @@ function unstake
 }
 
 
-if [[ "$PROPOSAL_STAKE" -le "$SEAT_PRICE_PROPOSALS" ]]
+if [[ "$PROPOSAL_STAKE" -lt "$SEAT_PRICE_PROPOSALS" ]]
 then
     echo "$PROPOSAL_STAKE is less than $SEAT_PRICE_PROPOSALS"
 
@@ -172,13 +250,19 @@ then
     echo "Validator Current Proposal = $PROPOSAL_STAKE" 
     echo "Seat Price Buffer = $SEAT_PRICE_BUFFER"
     SEAT_PRICE_DIFF=$((SEAT_PRICE_PROPOSALS - PROPOSAL_STAKE ))
-    if [ $SEAT_PRICE_DIFF -gt 500 ]
+    # If the difference between $SEAT_PRICE_PROPOSALS + $SEAT_PRICE_BUFFER - $PROPOSAL_STAKE is greater than 2000 increase stake by difference
+    # Since the buffer is greater than 10k this should not cause a problem 
+    # The price buffer is added to the Network Proposal Price before calculation so even if we are 4k under the price we are still 5k over the minimum
+    if [ $SEAT_PRICE_DIFF -gt 4500 ]
     then
     SEAT_PRICE_DIFF=$(echo \"$SEAT_PRICE_DIFF$ADD0\")
     stake $SEAT_PRICE_DIFF
     echo Stake increased by "$SEAT_PRICE_DIFF"
+    else
+    echo "The seat price difference of: $SEAT_PRICE_DIFF is not sufficent to trigger a transaction"
     fi
 fi
+
 
 # OPTION 2
 
@@ -193,15 +277,18 @@ then
     echo "Stake Diff: $SEAT_PRICE_DIFF"
     NEW_PROPOSAL_NUMBERS=$(echo $SEAT_PRICE_DIFF | sed 's/[^0-9]*//g')
     NEW_PROPOSAL_NUMBERS=$((SEAT_PRICE_DIFF + SEAT_PRICE_BUFFER))
-  
     if [[ "$NEW_PROPOSAL_NUMBERS" -gt 10000 ]]
     then
-    AMOUNT=\"$NEW_PROPOSAL_NUMBERS$ADD0\"
-    echo "Decreasing stake by: ${AMOUNT}"
-    unstake "$AMOUNT"
+      AMOUNT=\"$NEW_PROPOSAL_NUMBERS$ADD0\"
+      echo "Decreasing stake by: ${AMOUNT}"
+      unstake "$AMOUNT"
     fi
 fi
 
+if [[ "$PROPOSAL_STAKE" = "$SEAT_PRICE_PROPOSALS" ]]
+then
+echo "The proposal stake and seat price are equal"
+fi
 
 echo "Script Done"
 echo "----------- "
